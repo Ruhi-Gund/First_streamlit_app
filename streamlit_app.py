62b7565d999d282b955608743f1f18fe8f6a69fd
Ruhi-Gund <123539820+Ruhi-Gund@users.noreply.github.com>
2023-01-25 13:51:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
 
+streamlit.title('My Mom New Healty Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
