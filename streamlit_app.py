b26a6cc1da0b6b555ee3dc4461af46e2da0a684f
Ruhi-Gund <123539820+Ruhi-Gund@users.noreply.github.com>
2023-01-25 13:54:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 streamlit.title('My Parents New Healty Diner')
 
 streamlit.title('My Mom New Healty Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('Breakfast Favourite')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞Avocado Toast')
